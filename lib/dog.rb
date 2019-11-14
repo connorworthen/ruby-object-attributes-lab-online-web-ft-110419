365a4fac093c95ceda6168d005e7fabc2d4be8ce
connor worthen <connorw220@protonmail.com>
2019-11-14 19:30:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,3 +13,19 @@ class Dog
 end
 
 fido = Dog.new
+
+class Dog
+  def breed
+    @breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
+
+breed = Dog.new
