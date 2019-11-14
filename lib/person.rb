5bf2089f9116d8c65a84f9e12008ed19fee61e25
connor worthen <connorw220@protonmail.com>
2019-11-14 19:34:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -13,3 +13,19 @@ class Person
 end
 
 person = Person.new
+
+class Person
+  def job
+    @job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @name = name
+  end
+end
+
+job = Person.new
